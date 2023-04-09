0a5fac8a7d22d933b8cf29535d2795f6a6951f98
Boram Kim <boram2445@gmail.com>
2023-04-09 15:44:00 +0900

    Feat: 학교 순위 배너 컴포넌트 UI 구현

---
+++ src/components/home/bannerBox/BannerBox.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "./bannerBox.module.css";
+
+function BannerBox({ main, rank, name, correct }) {
+  return (
+    <div className={`${styles.container} ${main && styles.main}`}>
+      <p className={styles.rank}>
+        <span>{rank}</span>위
+      </p>
+      <strong className={styles.name}>{name}</strong>
+      <p className={styles.correct}>맞은 문제 수 : {correct}</p>
+    </div>
+  );
+}
+
+export default BannerBox;
