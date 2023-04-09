d10c4eddff5590bdde4ad9f22c89ce55707205a6
Boram Kim <boram2445@gmail.com>
2023-04-09 18:21:45 +0900

    Feat: 테이블 컴포넌트 UI 구현

---
+++ src/components/home/table/Table.jsx
@@ -0,0 +1,87 @@
+import React from "react";
+import { useTable } from "react-table";
+import styles from "./table.module.css";
+import silver1 from "../../../assets/icons/level/silver1.svg";
+
+function Table() {
+  const data = React.useMemo(() => dummyData, []);
+  const columns = React.useMemo(
+    () => [
+      { Header: "문제 번호", accessor: "id" },
+      { Header: "문제 제목", accessor: "title" },
+      {
+        Header: "난이도",
+        accessor: "difficulty",
+        Cell: () => <img src={silver1} alt="silver1" className={styles.icon} />,
+      },
+      { Header: "나의 도전 상태", accessor: "myState", Cell: () => <Button /> },
+      { Header: "외대 도전자 수", accessor: "challengerNum" },
+    ],
+    []
+  );
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({ columns, data });
+
+  return (
+    <table {...getTableProps()} className={styles.table}>
+      <thead>
+        {headerGroups.map(headerGroup => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map(column => (
+              <th {...column.getHeaderProps()} className={styles.th}>
+                {column.render("Header")}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map(row => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()} className={styles.tr}>
+              {row.cells.map(cell => {
+                return (
+                  <td {...cell.getCellProps()} className={styles.td}>
+                    {cell.render("Cell")}
+                  </td>
+                );
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
+
+export default Table;
+
+const dummyData = [
+  {
+    id: 1000,
+    title: "A+B",
+    difficulty: "silver1",
+    myState: false,
+    challengerNum: 123,
+  },
+  {
+    id: 1001,
+    title: "A+B",
+    difficulty: "silver1",
+    myState: false,
+    challengerNum: 123,
+  },
+  {
+    id: 1002,
+    title: "A+B",
+    difficulty: "silver1",
+    myState: false,
+    challengerNum: 123,
+  },
+];
+
+const Button = () => {
+  return <button>안녕</button>;
+};
