592f9983556d1aba740d2b2af428683919790746
Boram Kim <boram2445@gmail.com>
2023-04-13 17:22:23 +0900

    Design: 메인 페이지 테이블 정렬 박스 추가

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,8 +3,18 @@ import Banner from "../../components/home/banner/Banner";
 import RankBox from "../../components/home/rankBox/RankBox";
 import Table from "../../components/home/table/Table";
 import styles from "./home.module.css";
+import SelectBox from "../../components/common/selectBox/SelectBox";
 
 function Home() {
+  const selectList = [
+    "도전중인 문제",
+    "안푼 문제",
+    "쉬운 순",
+    "어려운 순",
+    "도전자 많은 순",
+    "도전자 적은 순",
+  ];
+
   return (
     <div className={styles.container}>
       <h2 className={`${styles.title} ${styles["max-width"]}`}>
@@ -18,7 +28,7 @@ function Home() {
         <div className={styles["table-wrapper"]}>
           <div>
             <h3 className={styles["table-title"]}>한국외대 미해결 문제</h3>
-            <p>선택 박스</p>
+            <SelectBox list={selectList} />
           </div>
           <Table />
         </div>
