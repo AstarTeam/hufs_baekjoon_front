2d48b213792518643f96f0ba6b53628eb3badd96
JunAhJang <junga5853@naver.com>
2023-04-28 15:19:43 +0900

    Feat: 헤더 오른쪽  텍스트 구현 및 레이아웃 추가

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Footer from "../common/footer/Footer";
+import Header from "../common/header/Header";
 
 function Layout() {
   return (
     <>
-      <header>헤더</header>
+      <Header />
       <main>
         <Outlet />
       </main>
