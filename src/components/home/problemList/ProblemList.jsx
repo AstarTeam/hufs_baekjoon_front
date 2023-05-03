a04146a4f5ffce99fe6cf26700730458b83d5727
Boram Kim <boram2445@gmail.com>
2023-05-04 00:00:40 +0900

    Feat: 문제 리스트 임시 api 연결

--- src/components/home/problemList/ProblemList.jsx
+++ src/components/home/problemList/ProblemList.jsx
@@ -1,25 +1,29 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+// import { sortDoingNow, sortNotStarted } from "../../../utils/problemSort";
 import Table from "../../common/table/Table";
 import PaginationBtn from "../../common/paginationBtn/PaginationBtn";
 import SelectBox from "../../common/selectBox/SelectBox";
-import styles from "./problemList.module.css";
-import silver1 from "../../../assets/icons/level/silver1.svg";
 import Button from "../../home/button/Button";
-import { sortDoingNow, sortNotStarted } from "../../../utils/problemSort";
+import styles from "./problemList.module.css";
+// import LevelIcon from "../levelIcon/LevelIcon";
 
 function ProblemList() {
-  const selectList = [
-    { id: 0, label: "정렬 방식" },
-    { id: 1, label: "도전중인 문제" },
-    { id: 2, label: "안푼 문제" },
-    { id: 3, label: "쉬운 순" },
-    { id: 4, label: "어려운 순" },
-    { id: 5, label: "도전자 많은 순" },
-    { id: 6, label: "도전자 적은 순" },
-  ];
+  const {
+    isLoading,
+    error,
+    data: problems,
+  } = useQuery(
+    ["problems"],
+    async () => {
+      console.log("fetching...");
+      return axios.get("/data/problems.json").then(res => res.data.problems);
+    },
+    { staleTime: 1000 * 60 * 5 }
+  );
 
-  const [problems, setProblems] = useState(dummyData); //전체 문제 데이타 - api를 통해 받아와야 함
-  const [sortedProblems, setSortedProblems] = useState(dummyData); //정렬된 문제 데이타
+  // const [sortedProblems, setSortedProblems] = useState([]); //정렬된 문제 데이타
 
   const [page, setPage] = useState(1);
   const [selected, setSelected] = useState(selectList[0]);
@@ -33,18 +37,21 @@ function ProblemList() {
       return result;
     }
   };
-  const sortDataHandler = problems => {
-    let sorted = problems;
-    if (selected.id === 1) sorted = sortDoingNow(problems);
-    else if (selected.id === 2) sorted = sortNotStarted(problems);
-    setSortedProblems(sorted);
-    setPage(1);
-  };
+  // const sortDataHandler = problems => {
+  //   let sorted = problems;
+  //   if (selected.id === 1) sorted = sortDoingNow(problems);
+  //   else if (selected.id === 2) sorted = sortNotStarted(problems);
+  //   setSortedProblems(sorted);
+  //   setPage(1);
+  // };
   const selectChangeHandler = item => setSelected(item);
 
-  useEffect(() => {
-    sortDataHandler(problems);
-  }, [selected.id]);
+  // useEffect(() => {
+  //   sortDataHandler(problems);
+  // }, [selected.id]);
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>{error}</p>;
 
   return (
     <div className={styles.container}>
@@ -57,15 +64,12 @@ function ProblemList() {
         />
       </div>
       <div className={styles["table-wrapper"]}>
-        <Table
-          dataList={postDataHandler(sortedProblems)}
-          columnList={columnList}
-        />
+        <Table dataList={postDataHandler(problems)} columnList={columnList} />
       </div>
       <PaginationBtn
         page={page}
         limit={limit}
-        totalNum={sortedProblems.length}
+        totalNum={problems.length}
         onPageChange={pageChangeHandler}
       />
     </div>
@@ -74,13 +78,24 @@ function ProblemList() {
 
 export default ProblemList;
 
+const selectList = [
+  { id: 0, label: "정렬 방식" },
+  { id: 1, label: "도전중인 문제" },
+  { id: 2, label: "안푼 문제" },
+  { id: 3, label: "쉬운 순" },
+  { id: 4, label: "어려운 순" },
+  { id: 5, label: "도전자 많은 순" },
+  { id: 6, label: "도전자 적은 순" },
+];
+
 const columnList = [
   { Header: "문제 번호", accessor: "id" },
   { Header: "문제 제목", accessor: "title" },
   {
     Header: "난이도",
     accessor: "difficulty",
-    Cell: () => <img src={silver1} alt="silver1" className={styles.icon} />,
+    // Cell: ({ cell: { value } }) => <LevelIcon level={value} />,
+    Cell: ({ cell: { value } }) => <div>{value}</div>,
   },
   {
     Header: "나의 도전 상태",
@@ -93,190 +108,9 @@ const columnList = [
       />
     ),
   },
-  { Header: "외대 도전자 수", accessor: "challengerNum" },
-];
-
-const dummyData = [
   {
-    id: 1000,
-    title: "A+B",
-    difficulty: "silver1",
-    myState: true,
-    challengerNum: 1,
-  },
-  {
-    id: 1001,
-    title: "A+B",
-    difficulty: "silver2",
-    myState: false,
-    challengerNum: 12,
-  },
-  {
-    id: 1002,
-    title: "A+B",
-    difficulty: "silver5",
-    myState: false,
-    challengerNum: 3,
-  },
-  {
-    id: 1003,
-    title: "A+B",
-    difficulty: "silver4",
-    myState: true,
-    challengerNum: 1,
-  },
-  {
-    id: 1004,
-    title: "A+B",
-    difficulty: "silver2",
-    myState: true,
-    challengerNum: 13,
-  },
-  {
-    id: 1005,
-    title: "A+B",
-    difficulty: "silver4",
-    myState: false,
-    challengerNum: 12,
-  },
-  {
-    id: 1006,
-    title: "A+B",
-    difficulty: "silver1",
-    myState: true,
-    challengerNum: 23,
-  },
-  {
-    id: 1007,
-    title: "A+B",
-    difficulty: "silver1",
-    myState: false,
-    challengerNum: 3,
-  },
-  {
-    id: 1008,
-    title: "A+B",
-    difficulty: "silver1",
-    myState: true,
-    challengerNum: 3,
-  },
-  {
-    id: 1009,
-    title: "A+B",
-    difficulty: "silver1",
-    myState: false,
-    challengerNum: 12,
-  },
-  {
-    id: 1010,
-    title: "A+B",
-    difficulty: "silver1",
-    myState: false,
-    challengerNum: 13,
-  },
-  {
-    id: 1011,
-    title: "A+B",
-    difficulty: "silver1",
-    myState: false,
-    challengerNum: 23,
-  },
-  {
-    id: 1012,
-    title: "A+B",
-    difficulty: "silver1",
-    myState: false,
-    challengerNum: 12,
-  },
-  {
-    id: 1013,
-    title: "A+B",
-    difficulty: "silver1",
-    myState: false,
-    challengerNum: 123,
-  },
-  {
-    id: 1014,
-    title: "A+B",
-    difficulty: "silver1",
-    myState: false,
-    challengerNum: 123,
-  },
-  {
-    id: 1015,
-    title: "A+B",
-    difficulty: "silver1",
-    myState: false,
-    challengerNum: 123,
-  },
-  {
-    id: 1016,
-    title: "A+B",
-    difficulty: "silver1",
-    myState: false,
-    challengerNum: 123,
-  },
-  {
-    id: 1017,
-    title: "A+B",
-    difficulty: "silver1",
-    myState: false,
-    challengerNum: 123,
-  },
-  {
-    id: 1018,
-    title: "A+B",
-    difficulty: "silver1",
-    myState: false,
-    challengerNum: 123,
-  },
-  {
-    id: 1019,
-    title: "A+B",
-    difficulty: "silver1",
-    myState: false,
-    challengerNum: 123,
-  },
-  {
-    id: 1020,
-    title: "A+B",
-    difficulty: "silver1",
-    myState: false,
-    challengerNum: 123,
-  },
-  {
-    id: 1021,
-    title: "A+B",
-    difficulty: "silver1",
-    myState: false,
-    challengerNum: 123,
-  },
-  {
-    id: 1022,
-    title: "A+B",
-    difficulty: "silver1",
-    myState: false,
-    challengerNum: 123,
-  },
-  {
-    id: 1023,
-    title: "A+B",
-    difficulty: "silver1",
-    myState: false,
-    challengerNum: 123,
-  },
-  {
-    id: 1024,
-    title: "A+B",
-    difficulty: "silver1",
-    myState: false,
-    challengerNum: 123,
-  },
-  {
-    id: 1025,
-    title: "A+B",
-    difficulty: "silver1",
-    myState: false,
-    challengerNum: 123,
+    Header: "외대 도전자 수",
+    accessor: "challengerNum",
+    Cell: () => 0,
   },
 ];
