a02c5c1e6f0314c0c517881762cbaa4eb0c90cdb
JunAhJang <junga5853@naver.com>
2023-05-07 22:14:17 +0900

    Style: 회원가입 폼 스타일 수정

--- src/pages/join/Join.jsx
+++ src/pages/join/Join.jsx
@@ -4,18 +4,29 @@ import styles from "./join.module.css";
 function Join() {
   return (
     <div className={styles["join-wrapper"]}>
-      <form className={styles["form-container"]}>
+      <div className={styles["title-form-container"]}>
         <p className={styles.title}>JOIN</p>
         <p className={styles.subtitle}>
           더 다양한 서비스 이용을 위해 회원가입을 진행해 주세요.
         </p>
-        <p className={styles.label}>아이디</p>
-        <input className={styles["form-input"]} placeholder="아이디" />
-        <input className={styles["form-input"]} placeholder="비밀번호" />
-        <button className={styles["form-button"]} type="submit">
-          로그인
-        </button>
-      </form>
+        <form className={styles["form-container"]}>
+          <p className={styles.label}>아이디</p>
+          <input className={`${styles["form-input"]} ${styles.id}`} />
+          <button className={styles["form-check-button"]}>중복확인</button>
+          <p className={styles.label}>닉네임</p>
+          <input className={styles["form-input"]} />
+          <p className={styles.label}>비밀번호</p>
+          <input className={styles["form-input"]} />
+          <p className={styles.label}>비밀번호 확인</p>
+          <input className={styles["form-input"]} />
+          <p className={styles.label}>이메일</p>
+          <input className={`${styles["form-input"]} ${styles.email}`} />
+          <button className={styles["form-check-button"]}>메일인증</button>
+          <button className={styles["form-button"]} type="submit">
+            가입하기
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
