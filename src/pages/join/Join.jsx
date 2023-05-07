6b957ff94aa8e4475f1524d87dae65dc11c589be
JunAhJang <junga5853@naver.com>
2023-05-07 19:24:04 +0900

    Feat: 로그인 hover 추가

--- src/pages/join/Join.jsx
+++ src/pages/join/Join.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import styles from "./join.module.css";
 
 function Join() {
-  return <div>Join</div>;
+  return (
+    <div className={styles["join-wrapper"]}>
+      <form className={styles["form-container"]}>
+        <p className={styles.title}>JOIN</p>
+        <p className={styles.subtitle}>
+          더 다양한 서비스 이용을 위해 회원가입을 진행해 주세요.
+        </p>
+        <p className={styles.label}>아이디</p>
+        <input className={styles["form-input"]} placeholder="아이디" />
+        <input className={styles["form-input"]} placeholder="비밀번호" />
+        <button className={styles["form-button"]} type="submit">
+          로그인
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default Join;
