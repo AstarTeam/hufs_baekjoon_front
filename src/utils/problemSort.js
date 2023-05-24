263834b6605a14fec4f515b1f25a46bae7f01651
Boram Kim <boram2445@gmail.com>
2023-05-24 21:38:42 +0900

    Remove: 불필요한 파일 제거

--- src/utils/problemSort.js
+++
@@ -1,7 +0,0 @@
-export const sortDoingNow = problems => {
-  return problems.filter(item => item.myState === true);
-};
-
-export const sortNotStarted = problems => {
-  return problems.filter(item => item.myState === false);
-};
