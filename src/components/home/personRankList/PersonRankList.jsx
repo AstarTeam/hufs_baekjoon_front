44d353c637ad8aaacef7486604bf09bef9cfd79b
Boram Kim <boram2445@gmail.com>
2023-05-28 19:53:51 +0900

    Feat: hufs 명예의 전당 api 연결

--- src/components/home/personRankList/PersonRankList.jsx
+++ src/components/home/personRankList/PersonRankList.jsx
@@ -1,9 +1,33 @@
 import React from "react";
-import hufsRank from "../../../assets/icons/hufs_rank.svg";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 import Table from "../../common/table/Table";
+import Loading from "../../common/loading/Loading";
+import hufsRank from "../../../assets/icons/hufs_rank.svg";
 import styles from "./personRankList.module.css";
 
+async function getRankList() {
+  const url = "/fame";
+  const res = await axios(url);
+
+  const sortedData = res.data.userList
+    .sort((a, b) => b.count - a.count)
+    .map((item, index) => ({ ...item, rank: index + 1 }));
+  return sortedData;
+}
+
 function PersonRankList() {
+  const {
+    isLoading,
+    error,
+    data: rankers,
+  } = useQuery(["fame"], getRankList, {
+    staleTime: 1000 * 60 * 5,
+  });
+
+  if (isLoading) return <Loading />;
+  if (error) return <p>{error}</p>;
+
   return (
     <div className={styles.container}>
       <img src={hufsRank} alt="HUFS 명예의 전당" />
@@ -12,12 +36,12 @@ function PersonRankList() {
       </p>
       <div className={styles["table-wrapper"]}>
         <Table
-          dataList={dummyData1}
+          dataList={rankers.slice(0, 5)}
           columnList={columnList}
           type="personRank1"
         />
         <Table
-          dataList={dummyData2}
+          dataList={rankers.slice(5)}
           columnList={columnList}
           type="personRank2"
         />
@@ -27,21 +51,6 @@ function PersonRankList() {
 }
 
 export default PersonRankList;
-
-const dummyData1 = [
-  { rank: 1, nick_name: "내가 백준 최고다", num: 4 },
-  { rank: 2, nick_name: "외대 최강", num: 2 },
-  { rank: 3, nick_name: "노어 23학번", num: 2 },
-  { rank: 4, nick_name: "하하", num: 1 },
-  { rank: 5, nick_name: "안녕하세요", num: 1 },
-];
-const dummyData2 = [
-  { rank: 6, nick_name: "백준 왕초보", num: 1 },
-  { rank: 7, nick_name: "미컴 12", num: 1 },
-  { rank: 8, nick_name: "외대짱", num: 1 },
-  { rank: 9, nick_name: "여름", num: 1 },
-  { rank: 10, nick_name: "안녕", num: 1 },
-];
 
 const columnList = [
   {
@@ -53,6 +62,21 @@ const columnList = [
       >{`#${value}`}</span>
     ),
   },
-  { Header: "닉네임", accessor: "nick_name" },
-  { Header: "푼 문제 수", accessor: "num" },
+  { Header: "닉네임", accessor: "name" },
+  { Header: "푼 문제 수", accessor: "count" },
 ];
+
+// const dummyData1 = [
+//   { rank: 1, name: "내가 백준 최고다", count: 4 },
+//   { rank: 2, name: "외대 최강", count: 2 },
+//   { rank: 3, name: "노어 23학번", count: 2 },
+//   { rank: 4, name: "하하", count: 1 },
+//   { rank: 5, name: "안녕하세요", count: 1 },
+// ];
+// const dummyData2 = [
+//   { rank: 6, name: "백준 왕초보", count: 1 },
+//   { rank: 7, name: "미컴 12", count: 1 },
+//   { rank: 8, name: "외대짱", count: 1 },
+//   { rank: 9, name: "여름", count: 1 },
+//   { rank: 10, name: "안녕", count: 1 },
+// ];
