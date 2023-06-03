6e88d455a7f1d421271dc3d55a790ad512206d5c
Boram Kim <boram2445@gmail.com>
2023-06-03 15:37:29 +0900

    Feat: 문제 리스트 받기 token 유무 api 구별

--- src/api/home.js
+++ src/api/home.js
@@ -1,10 +1,18 @@
 import axios from "axios";
 
 //문제 리스트 받기
-export async function getProblemList(page, type, num) {
-  const url = `/data/problems${page - 1}.json`; //sampleUrl
-  // const url = `/${type}/?page=${page - 1}&size=15`;
-  const res = await axios(url);
+export async function getProblemList(page, type, token) {
+  // const url = `/data/problems${page - 1}.json`; //sampleUrl
+  const url = `/${type}${token ? "/token" : ""}?page=${page - 1}&size=15`;
+  const config = token
+    ? {
+        url,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    : { url };
+  const res = await axios(config);
   return res.data;
 }
 
@@ -17,4 +25,12 @@ export async function getRankList() {
     .sort((a, b) => b.count - a.count)
     .map((item, index) => ({ ...item, rank: index + 1 }));
   return sortedData;
+}
+
+//도전자수 갱신
+export async function postChallenge(problem_num, user_id) {
+  const url = `/problem/${problem_num}/${user_id}/challenge `;
+  const res = await axios.post(url);
+
+  console.log(res.data);
 }
