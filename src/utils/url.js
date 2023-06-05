48f265aaf0e45a60be1942b7094933c33385efa6
Boram Kim <boram2445@gmail.com>
2023-06-05 19:53:32 +0900

    Fix: BASE URL 변경

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,3 +1,3 @@
-// export const BASE_URL = "http://43.201.190.128:8000"; //실제 서버
+// export const BASE_URL = "http://43.201.190.128"; //실제 서버
 
 export const BASE_URL = "http://localhost:8000"; //로컬 서버
