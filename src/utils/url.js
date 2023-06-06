7cdd31bd519025ab38feaa33367abf4c5f34ab37
Boram Kim <boram2445@gmail.com>
2023-06-06 10:31:18 +0900

    Fix: mixed content 오류로 인한 서버 연결 해결

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,3 +1,6 @@
-export const BASE_URL = "http://43.201.190.128"; //실제 서버
+const host =
+  window.location.hostname === "localhost" ? "http://43.201.190.128" : "api";
 
-// export const BASE_URL = "http://localhost:8000"; //로컬 서버
+export const BASE_URL = host;
+
+// export const BASE_URL = "http://localhost:8000"; //백엔드 로컬 서버
