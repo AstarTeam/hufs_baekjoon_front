0b0f2235a37f3e6fa199264167b6ade93a429577
Boram Kim <boram2445@gmail.com>
2023-06-08 15:23:43 +0900

    Fix: 아이콘 순서 변경

--- src/components/home/levelIcon/LevelIcon.jsx
+++ src/components/home/levelIcon/LevelIcon.jsx
@@ -14,36 +14,36 @@ function LevelIcon({ level }) {
 }
 
 const levelList = [
-  { id: 1, level: "bronze1" },
-  { id: 2, level: "bronze2" },
-  { id: 3, level: "bronze3" },
-  { id: 4, level: "bronze4" },
-  { id: 5, level: "bronze5" },
-  { id: 6, level: "silver1" },
-  { id: 7, level: "silver2" },
+  { id: 1, level: "bronze5" }, //5
+  { id: 2, level: "bronze4" }, //4
+  { id: 3, level: "bronze3" }, //3
+  { id: 4, level: "bronze2" }, //2
+  { id: 5, level: "bronze1" }, //1
+  { id: 6, level: "silver5" },
+  { id: 7, level: "silver4" },
   { id: 8, level: "silver3" },
-  { id: 9, level: "silver4" },
-  { id: 10, level: "silver5" },
-  { id: 11, level: "gold1" },
-  { id: 12, level: "gold2" },
+  { id: 9, level: "silver2" },
+  { id: 10, level: "silver1" },
+  { id: 11, level: "gold5" },
+  { id: 12, level: "gold4" },
   { id: 13, level: "gold3" },
-  { id: 14, level: "gold4" },
-  { id: 15, level: "gold5" },
-  { id: 16, level: "platinum1" },
-  { id: 17, level: "platinum2" },
+  { id: 14, level: "gold2" },
+  { id: 15, level: "gold1" },
+  { id: 16, level: "platinum5" },
+  { id: 17, level: "platinum4" },
   { id: 18, level: "platinum3" },
-  { id: 19, level: "platinum4" },
-  { id: 20, level: "platinum5" },
-  { id: 21, level: "diamond1" },
-  { id: 22, level: "diamond2" },
+  { id: 19, level: "platinum2" },
+  { id: 20, level: "platinum1" },
+  { id: 21, level: "diamond5" },
+  { id: 22, level: "diamond4" },
   { id: 23, level: "diamond3" },
-  { id: 24, level: "diamond4" },
-  { id: 25, level: "diamond5" },
-  { id: 26, level: "ruby1" },
-  { id: 27, level: "ruby2" },
+  { id: 24, level: "diamond2" },
+  { id: 25, level: "diamond1" },
+  { id: 26, level: "ruby5" },
+  { id: 27, level: "ruby4" },
   { id: 28, level: "ruby3" },
-  { id: 29, level: "ruby4" },
-  { id: 30, level: "ruby5" },
+  { id: 29, level: "ruby2" },
+  { id: 30, level: "ruby1" },
 ];
 
 export default LevelIcon;
